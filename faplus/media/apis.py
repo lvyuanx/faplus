7d3dec2b0ef36078e414844760452a97ee910e26
lyx <2466057319@qq.com>
2025-01-08 21:32:07 +0800

    媒体测试接口加入DEBUG组

--- faplus/media/apis.py
+++ faplus/media/apis.py
@@ -7,6 +7,6 @@ if settings.DEBUG:
     from .views.tests import test_upload_view, test_download_view
 
     apis["/debug"] = [
-        ("01", "/upload", test_upload_view, "测试文件上传"),
-        ("02", "/donwload/{sn}", test_download_view, "测试文下载"),
+        ("01", "/upload", test_upload_view, "测试文件上传", {"tags": ["DEBUG"]}),
+        ("02", "/donwload/{sn}", test_download_view, "测试文下载", {"tags": ["DEBUG"]}),
     ]
