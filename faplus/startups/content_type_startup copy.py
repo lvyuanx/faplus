c51f95821a060aa088824a9eddbbbe3d7fb88000
lyx <2466057319@qq.com>
2025-01-09 22:34:48 +0800

    fix: 修复初始化contentType不正确的bug
    由于使用了错误的数据结构，导致一个app只能保存一个模型。

---
+++ faplus/startups/content_type_startup copy.py
@@ -0,0 +1,44 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : content_type_startup.py
+# Time       : 2025-01-05 23:26:21
+# Author     : lyx
+# version    : python 3.11
+# Description: 启动时初始化内容类型
+"""
+import importlib
+import inspect
+
+from faplus.utils import get_setting_with_default
+from tortoise.models import Model
+from faplus.models import ContentType
+from collections import defaultdict
+
+
+def content_type_register_event(**kwargs):
+
+    async def do():
+        FAP_INSERTAPPS = get_setting_with_default("FAP_INSERTAPPS")
+        model_dict = defaultdict(list)
+        for app in FAP_INSERTAPPS:
+            try:
+                app_models = importlib.import_module(f"{app}.models")
+            except ModuleNotFoundError:
+                continue
+            
+            for _, obj in inspect.getmembers(app_models):
+                # 判断对象是否是类，并且是 Model 的子类
+                if obj is not Model and inspect.isclass(obj) and issubclass(obj, Model):
+                    model_dict[app.rsplit(".")[-1]].append(obj.__name__)
+        
+        
+
+        for app, models in model_dict.items():
+            for model in models:
+                await ContentType.get_or_create(
+                    app_label=app,
+                    model=model,
+                )
+
+    return do
