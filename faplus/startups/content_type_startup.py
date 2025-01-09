67feec19d56edc616b82e85370006d3876c9230b
lyx <2466057319@qq.com>
2025-01-09 22:41:01 +0800

    fix: 文件提交错误

--- faplus/startups/content_type_startup.py
+++ faplus/startups/content_type_startup.py
@@ -8,32 +8,37 @@
 # Description: 启动时初始化内容类型
 """
 import importlib
+import inspect
 
 from faplus.utils import get_setting_with_default
 from tortoise.models import Model
 from faplus.models import ContentType
+from collections import defaultdict
 
 
 def content_type_register_event(**kwargs):
 
     async def do():
         FAP_INSERTAPPS = get_setting_with_default("FAP_INSERTAPPS")
-        model_dict = {}
+        model_dict = defaultdict(list)
         for app in FAP_INSERTAPPS:
             try:
                 app_models = importlib.import_module(f"{app}.models")
             except ModuleNotFoundError:
                 continue
+            
+            for _, obj in inspect.getmembers(app_models):
+                # 判断对象是否是类，并且是 Model 的子类
+                if obj is not Model and inspect.isclass(obj) and issubclass(obj, Model):
+                    model_dict[app.rsplit(".")[-1]].append(obj.__name__)
+        
+        
 
-            # 获取所有模型
-            for model in app_models.__dict__.values():
-                if isinstance(model, type) and issubclass(model, Model):
-                    model_dict[app.rsplit(".")[-1]] = model.__name__
-
-        for app, model in model_dict.items():
-            await ContentType.get_or_create(
-                app_label=app,
-                model=model,
-            )
+        for app, models in model_dict.items():
+            for model in models:
+                await ContentType.get_or_create(
+                    app_label=app,
+                    model=model,
+                )
 
     return do
