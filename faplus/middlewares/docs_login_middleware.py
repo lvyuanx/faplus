147e1550eb25e60af10c9c82bfce06253bf24874
lyx <2466057319@qq.com>
2025-01-10 22:51:12 +0800

    feat: 支持文档登录后转成jwt-token，无需在测试稳定中手动输入token

--- faplus/middlewares/docs_login_middleware.py
+++ faplus/middlewares/docs_login_middleware.py
@@ -11,6 +11,7 @@ import logging
 
 from fastapi import Request, Response
 from fastapi.responses import RedirectResponse
+from starlette.datastructures import MutableHeaders
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
 from faplus.utils import get_setting_with_default
@@ -20,7 +21,9 @@ from starlette.requests import cookie_parser
 
 FAP_DOCS_URL = get_setting_with_default("FAP_DOCS_URL")
 FAP_REDOC_URL = get_setting_with_default("FAP_REDOC_URL")
+FAP_OPENAPI_URL = get_setting_with_default("FAP_OPENAPI_URL")
 DEBUG = get_setting_with_default("DEBUG")
+FAP_TOKEN_TAG = get_setting_with_default("FAP_TOKEN_TAG")
 
 logger = logging.getLogger(__package__)
 
@@ -28,13 +31,18 @@ class DocsLoginMiddleware(BaseHTTPMiddleware):
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
     ) -> Response:
-        
         path = request.url.path
-        if not path.startswith(FAP_DOCS_URL) and not path.startswith(FAP_REDOC_URL):
+        if not path.startswith(FAP_DOCS_URL) and not path.startswith(FAP_REDOC_URL) and not path.startswith(FAP_OPENAPI_URL):
             return await call_next(request)
         
-        tk = request.cookies.get("doc-token")
-        if tk and await token_util.verify_token(tk):  # 验证token
+        tk = request.cookies.get(FAP_TOKEN_TAG)
+        if tk:  # 验证token, 直接
+            # 创建一个可变的 headers 副本
+            headers = MutableHeaders(scope=request.scope)
+            headers[FAP_TOKEN_TAG] = tk
+
+            # 将新的 headers 应用到 request.scope 中
+            request.scope["headers"] = headers.raw
             return await call_next(request)
         
         query = request.query_params
@@ -43,14 +51,14 @@ class DocsLoginMiddleware(BaseHTTPMiddleware):
         if username and password:
             try:
                 user = await user_util.authenticate_user(username=username, password=password)
+                print(user)
                 if user:
-                    token = await token_util.create_token({"doc": ""})
+                    token = await token_util.create_token({"uid": user["id"]})
                     response = RedirectResponse(url=path, status_code=302)
-                    response.set_cookie(key="doc-token", value=token, httponly=DEBUG)
+                    response.set_cookie(key=FAP_TOKEN_TAG, value=token, httponly=DEBUG)
                     return response
             except Exception as e:
                 logging.warning("", exc_info=True)
-        
         return Response(status_code=401)
 
         
--- faplus/middlewares/whitelist_middleware.py
+++ faplus/middlewares/whitelist_middleware.py
@@ -21,16 +21,10 @@ logger = logging.getLogger(__package__)
 
 FAP_JWT_WHITES = get_setting_with_default("FAP_JWT_WHITES")
 FAP_LOGIN_URL = get_setting_with_default("FAP_LOGIN_URL")
-FAP_DOCS_URL = get_setting_with_default("FAP_DOCS_URL")
-FAP_REDOC_URL = get_setting_with_default("FAP_REDOC_URL")
-FAP_OPENAPI_URL = get_setting_with_default("FAP_OPENAPI_URL")
 
-# 白名单 + 登录地址 + docs地址 + redoc地址 + openapi地址 予以放行
+# 白名单 + 登录地址
 whitelist = FAP_JWT_WHITES + [
-    FAP_LOGIN_URL,
-    FAP_DOCS_URL,
-    FAP_REDOC_URL,
-    FAP_OPENAPI_URL,
+    FAP_LOGIN_URL
 ]
 
 
